82166f3ce9b3d121a2fc960203e6a1c0a5860997
Joel Castillo <latinreve@gmail.com>
2021-04-02 20:34:13 -0400

    Revert "Initial commit"
    This reverts commit cd491c9189376b60f83186c9b0c630e89c3a90ef.

--- index.js
+++
@@ -1,5 +0,0 @@
-const StripComments = require("./4kyu/StripComments");
-const UrlDomainName = require("./5kyu/UrlDomainName");
-
-if(StripComments()) console.log("Sample tests passed for StripComments")
-if(UrlDomainName()) console.log("Sample tests passed for UrlDomainName")
